c1d712f8792f580ef9943d9fe3d6072cb642499d
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-07-24 20:18:28 +0300

    tr
    add transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import user from "../source/user.json";
+import data from "../source/data.json";
+import friends from '../source/friends.json';
+import transactions from '../source/transactions.json';
+
+import { TransactionHistory } from './transaction/Transaction';
+import { FriendList } from './friends/FriendsList';
+import { Statistics } from './stats/Statisctics';
+import { Profile }from './prof/Profile'; 
+
+import React from 'react';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        margin: 'auto',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'baseline',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        maxWidth: '1500px',
+        flexWrap: 'wrap',
+        marginBottom: '40px',
+        marginTop: '40px'
       }}
     >
-      React homework template
+      <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+      />
+      <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={data} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
